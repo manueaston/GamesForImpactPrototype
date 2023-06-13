9516fc8d785cb4f836ccd0124069dab29ff7ad2f
ManuE <manueaston@gmail.com>
2023-06-13 21:15:35 +1200

    Social Media Window
    Scrolling implemented
    Also fixed window dragging to not jump to centre of window

---
+++ GamesForImpactPrototype/Assets/Scripts/Scroll.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Class for making objects be able to scroll on mouse drag.
+// Only scrolls in y direction
+public class Scroll : MonoBehaviour 
+{
+    Vector3 scrollWindowSize;
+    bool canScroll;
+
+    public GameObject content;
+    Vector3 contentPos;
+    SpriteRenderer contentRenderer;
+    float contentLength;
+
+    private Vector3 mouseDisplacement; // Displacement of mouse holding from centre of window
+
+    private void Start()
+    {
+        scrollWindowSize = GetComponent<Collider2D>().bounds.size;
+        canScroll = false;
+
+        contentRenderer = content.GetComponent<SpriteRenderer>();
+        contentLength = contentRenderer.size.y;
+        contentPos = content.transform.position;
+    }
+
+    // Called when user first clicks on collider
+    private void OnMouseDown()
+    {
+        mouseDisplacement = content.transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition); // object pos - mouse pos
+        canScroll = true;
+    }
+
+    private void OnMouseExit()
+    {
+        // Stop dragging when mouse leaves collider
+        canScroll = false;
+    }
+
+    // Called when user drags mouse click over collider (title bar)
+    void OnMouseDrag()
+    {
+        if (canScroll)
+        {
+            contentPos = content.transform.position;
+
+            // get mouse position, and move object Y
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            contentPos.y = mousePos.y + mouseDisplacement.y;
+            content.transform.position = contentPos;
+
+            CheckBounds();
+        }
+    }
+
+    public void UpdateContent()
+    {
+        contentLength = contentRenderer.size.y;
+    }
+
+    void CheckBounds()
+    {
+        // keep content within boundaries of sprite
+
+        if (contentPos.y + (contentLength / 2) > transform.position.y + (scrollWindowSize.y / 2))
+        {
+            content.transform.position = new Vector3(contentPos.x, transform.position.y + (scrollWindowSize.y / 2) - (contentLength / 2), contentPos.z);
+        }
+        else if (contentPos.y - (contentLength / 2) < transform.position.y - (scrollWindowSize.y / 2))
+        {
+            content.transform.position = new Vector3(contentPos.x, transform.position.y - (scrollWindowSize.y / 2) + (contentLength / 2), contentPos.z);
+        }
+    }
+}
--- GamesForImpactPrototype/Assets/Scripts/Window.cs
+++ GamesForImpactPrototype/Assets/Scripts/Window.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 
 public class Window : MonoBehaviour
 {
+    private Vector3 mouseDisplacement; // Displacement of mouse holding from centre of window
+    // Stores displacement needed to stop window centre from snapping to mouse when being dragged
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,11 +20,17 @@ public class Window : MonoBehaviour
         
     }
 
+    // Called when user first clicks on collider
+    private void OnMouseDown()
+    {
+        mouseDisplacement = transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition); // window pos - mouse pos
+    }
+
     // Called when user drags mouse click over collider (title bar)
     void OnMouseDrag()
     {
         // get mouse position, and move window X and Y (not Z)
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        transform.position = new Vector3(mousePos.x, mousePos.y, transform.position.z);
+        transform.position = new Vector3(mousePos.x + mouseDisplacement.x, mousePos.y + mouseDisplacement.y, transform.position.z); // Adds mouse displacement in x and y dimensions
     }
 }
