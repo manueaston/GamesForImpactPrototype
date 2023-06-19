1943a4a2101363c00775dabeaab6d51a2d7f0b19
draconix6 <30545768+draconix6@users.noreply.github.com>
2023-06-19 20:50:13 +1200

    chat window improvements

--- GamesForImpactPrototype/Assets/Scripts/Buttons/ButtonSendMessage.cs
+++ GamesForImpactPrototype/Assets/Scripts/Buttons/ButtonSendMessage.cs
@@ -7,17 +7,17 @@ public class ButtonSendMessage : ButtonParent
     public ChatWindow chat;
 
     // emotes
-    private static int emoteCount = 6;
+    // private static int emoteCount = 6;
     // public Sprite emote1, emote2, emote3, emote4, emote5, emote6;
-    public Sprite[] emotes = new Sprite[emoteCount];
-    private Sprite currentEmote;
+    // public Sprite[] emotes = new Sprite[emoteCount];
+    public Sprite currentEmote;
 
     public override void ButtonPressed()
     {
         // send a message to chat window
         // picks a random emote sprite to send
         chat.AddOutgoingMessage(currentEmote);
-        currentEmote = emotes[Random.Range(0, emoteCount - 1)];
-        spriteRenderer.sprite = currentEmote;
+        // currentEmote = emotes[Random.Range(0, emoteCount - 1)];
+        // spriteRenderer.sprite = currentEmote;
     }
 }
